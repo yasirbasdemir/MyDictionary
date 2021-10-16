4d83c0dfa0d907088b7ff3137a4f3427fcfe7e0a
yasir <yasir@DESKTOP-6QSJ7FE>
2021-10-17 02:55:38 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T> // Generic Class
+    {
+        T[] array; // T tipinde array oluşturdum.
+        T[] tempArray; // T tipinde geçiçi array (tempArray) oluşturdum.
+        
+        public MyDictionary()
+        {
+            array = new T[0];  //  0 elemanlı array oluşturdum.
+        }
+
+        public void Add(T item)
+        {
+            tempArray = array;  //  T tipin de oluşturduğum geçiçi array (tempArray)i array in referansını tutturdum.
+            array = new T[array.Length + 1];  // array 'in eleman sayısını 1 arttırıyorum.
+            for (int i = 0; i < tempArray.Length; i++) // geçiçi array(tempArray)in eleman sayısını döngüde arttırarak tekrar array'e aktardım .
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = item;  // Gönderdiğimiz yeni elemanı da arrayin son elemanına eklemiş oldum.
+
+        }
+        
+
+        public int Count  //  Eğer ki MyDictionary den Count'u çağırırsam bu bana array in Length ini yani eleman sayısını vericek.
+        {
+            get { return array.Length; }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+   // ÖDEV KONUSU  :
+   // Aynen derste yaptığımız MyList gibi siz de kendi Dictonary sınıfınızı yazınız. (MyDictionary)
+   // Sadece ekleme(Add) metodunu yazınız.
+   // Yazdığınız kodu GitHub sayfanızı atıp yorum kısmına adresini ekleyiniz.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> fullName = new MyDictionary<string>();
+            fullName.Add("Yasir Başdemir");
+            fullName.Add("Murat Başdemir");
+            fullName.Add("Muhammed Hasan Başdemir");
+            Console.WriteLine(fullName.Count);
+        }
+    }
+}
